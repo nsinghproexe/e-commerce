4ca269b571ac22711a2a3904711f93d02260ac73
nsinghproexe <119045598+nsinghproexe@users.noreply.github.com>
2022-11-25 17:05:35 +0530

    Add files via upload

---
+++ e-commerce-app/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App text-3xl font-bold underline">
+      Let's start Deployment
+    </div>
+  );
+}
+
+export default App;
---
+++ e-commerce-app/src/Redux/Store.js
@@ -0,0 +1,19 @@
+import { configureStore } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage'
+import { combineReducers } from 'redux'
+import {persistReducer } from 'redux-persist'
+
+const reducers = combineReducers({
+})
+const persistConfig = {
+    key: 'root',
+    storage
+}
+const persistedReducer = persistReducer(persistConfig, reducers)
+
+const store = configureStore({
+    reducer: persistedReducer,
+    devTools: process.env.NODE_ENV !== 'production'
+})
+
+export default store
